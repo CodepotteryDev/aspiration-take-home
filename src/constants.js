fc4d35f3244ee41d99358e12e923a6ee22fbcb33
Eduardo Elias Valenzuela Rodriguez <geekintheflesh@gmail.com>
2021-12-08 20:17:57 -0600

    Create constant file with api root, git docs, default topic and search offset

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const GITH_GRAPHQL_API_URL = 'https://api.github.com/graphql';
+export const GIT_HUB_TOKEN_DOCS = 'https://docs.github.com/en/enterprise-server@3.0/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token';
+export const HOME_TOPIC = 'react';
+export const OFFSET_INCREMENT = 10;
